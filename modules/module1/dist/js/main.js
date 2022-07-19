f99e070c176c34f1cb9f386b96d5cbafd9ab1018
ViliAu <36374215+ViliAu@users.noreply.github.com>
2022-07-19 17:05:19 +0300

    Module 4 updates

--- modules/module1/dist/js/main.js
+++ modules/module1/dist/js/main.js
@@ -3,7 +3,7 @@ const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
 
-const navItems = document.querySelectorAll('.nav-items');
+const navItems = document.querySelectorAll('.nav-item');
 
 menuBtn.addEventListener('click', () => {
     menuBtn.classList.toggle('close');
